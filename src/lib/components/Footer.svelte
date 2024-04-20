0c259c00d1a668fd2de8b72c4eec4967ecd8e3f0
LucasCatolino <19626573+LucasCatolino@users.noreply.github.com>
2024-04-20 16:38:58 -0300

    Change: clean footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,12 +1,3 @@
-<!--div class="flex flex-col min-h-screen"-->
-    <!-- Main content -->
-    <!--div class="flex-grow"-->
-      <!-- Your main content here -->
-    <!--/div-->
-  
-    <!-- Footer -->
-    <footer class="bg-gray-200 py-4 text-center">
-      <p class="text-gray-600">© 2024 Lucas Catolino. All rights reserved.</p>
-    </footer>
-  <!--/div-->
-  
+<footer class="bg-gray-200 py-4 text-center">
+  <p class="text-gray-600">© 2024 Lucas C. All rights reserved.</p>
+</footer>
