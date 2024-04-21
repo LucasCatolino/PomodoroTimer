83f2b91951338562928d61f3c9d0f2b4f59c1faf
LucasCatolino <19626573+LucasCatolino@users.noreply.github.com>
2024-04-20 22:55:02 -0300

    Change: add server to get the pomodoros from a dummy database

---
+++ src/routes/study/+page.server.ts
@@ -0,0 +1,21 @@
+import type { ServerLoadEvent } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types"
+
+export const load: PageServerLoad = async (event) => {   
+    let cards = await fetchCards(event);
+    
+    return {
+        cards
+    }
+}
+
+export type Cards = {id: number, title: string, studyTime: number, restTime: number, done: boolean};
+
+async function fetchCards(event: ServerLoadEvent) {
+    const res = await event.fetch('/study');
+    const data = await res.json();
+
+    if (res.ok) {
+        return data as Cards[];
+    }
+}
---
+++ src/routes/study/+server.ts
@@ -0,0 +1,43 @@
+const STUDY_TIME: number = 25;
+const REST_TIME: number = 5;
+
+export async function GET() {
+    // connect to db and get data
+    const studyPomodoros = [
+        {
+            id: 1,
+            title: "Pomodoro block",
+            studyTime: STUDY_TIME,
+            restTime: REST_TIME,
+            done: false,
+        },
+        {
+            id: 2,
+            title: "Pomodoro block",
+            studyTime: STUDY_TIME,
+            restTime: REST_TIME,
+            done: false,
+        },
+        {
+            id: 3,
+            title: "Pomodoro block",
+            studyTime: STUDY_TIME,
+            restTime: REST_TIME,
+            done: false,
+        },
+        {
+            id: 4,
+            title: "Pomodoro block",
+            studyTime: STUDY_TIME,
+            restTime: REST_TIME,
+            done: false,
+        }
+    ]
+
+    return new Response(JSON.stringify(studyPomodoros), {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    
+}
