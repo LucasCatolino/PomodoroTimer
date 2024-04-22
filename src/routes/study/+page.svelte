bc62f758b8fdab19ae33a44c1d92186842351e44
LucasCatolino <19626573+LucasCatolino@users.noreply.github.com>
2024-04-22 00:39:23 -0300

    Change: minor styling refactor

--- src/routes/study/+page.svelte
+++ src/routes/study/+page.svelte
@@ -6,17 +6,23 @@
   const cards = writable(data);
 </script>
 
-<div class="flex flex-col items-center justify-center h-screen ">
+<div class="study min-h-screen flex flex-col items-center justify-center h-screen ">
 
   <PomodoroBlocks data={$cards}/>
 
 </div>
   
 <style>
-    .glass {
-      background: rgba(255, 235, 233, 0.15);
-      box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-      backdrop-filter: blur(5px);
-      border-radius: 10px;
-    }
+  .glass {
+    background: rgba(255, 235, 233, 0.15);
+    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+    backdrop-filter: blur(5px);
+    border-radius: 10px;
+  }
+
+  .study {
+      text-align: center;
+      display: block;
+      margin: 20px auto;
+  }
 </style>
